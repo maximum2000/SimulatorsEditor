318f0527cb87cb4b4c5de701d848a10f013b6cfd
max gammer <gammermax@gmail.com>
2022-10-18 20:47:55 +0500

    +

--- src/editor/main.cpp
+++ src/editor/main.cpp
@@ -101,8 +101,7 @@ int main(int, char**)
         ImGui::NewFrame();
 
         // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
+        if (show_demo_window) ImGui::ShowDemoWindow(&show_demo_window);
 
         // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
         {
