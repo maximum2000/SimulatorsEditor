7601c549979d92776d6c06a5aed4c233003f14d0
baturinma <baturin.mihail.a@gmail.com>
2022-11-28 15:20:42 +0500

    28.11

--- src/editor/scenariosgui.cpp
+++ src/editor/scenariosgui.cpp
@@ -24,6 +24,7 @@ namespace ScenarioGUI {
     void CleanupDeviceD3D();
     void CreateRenderTarget();
     void CleanupRenderTarget();
+    void DrawObjects();
     void WorkspaceInitialization();
     LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
@@ -149,6 +150,7 @@ namespace ScenarioGUI {
             ImGui::SetNextWindowPos(ImVec2(viewport->WorkPos.x, (viewport->WorkSize.y / 3) - 2));
             ImGui::SetNextWindowSize(ImVec2(viewport->WorkSize.x / 4, viewport->WorkSize.y / 3));
             ImGui::Begin(u8"Элементы сценария", NULL, ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize);
+            DrawObjects();
             ImGui::End();
             ImGui::SetNextWindowPos(ImVec2(viewport->WorkPos.x, (2 * viewport->WorkSize.y / 3) - 4));
             ImGui::SetNextWindowSize(ImVec2(viewport->WorkSize.x / 4, (viewport->WorkSize.y / 3) + 33));
@@ -220,6 +222,12 @@ namespace ScenarioGUI {
         // Возвращаем старый стиль отступов
         ImGui::PopStyleVar();
         ImGui::End();
+    }
+
+    void DrawObjects()
+    {
+        //ImDrawList* draw_list = ImGui::GetWindowDrawList();
+        //ImGui::ImageButton();
     }
 
     bool CreateDeviceD3D(HWND hWnd)
