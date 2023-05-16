93a2bc4ec510295d2e4ed2e98015c33e28aa6158
maxgammer <maxgammer@gmail.com>
2023-05-16 17:24:55 +0500

    +

--- src/MM10/Assets/MM10.cs
+++ src/MM10/Assets/MM10.cs
@@ -1,34 +1,141 @@
     using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MM10 : MonoBehaviour
 {
+    public int maxx = 80;
+    public int maxy = 80;
+
+
     public MyElementUI prefab;
     //public GameObject parent;
     public float dx;
     public float dy;
 
+    public TextMeshPro info_text;
+
+    private List<MyElementUI> map2d;
+
+    public enum ModeType
+    {
+        none,
+        wall,
+        gas,
+        fluid,
+        solid,
+        delete
+    }
+
+    public ModeType selectedType;
+
+
+    public void CellClick(ref MyElementUI cell)
+    {
+        if (selectedType == ModeType.none)
+        {
+            //cell.gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 0f, 0);
+        }
+        else if (selectedType == ModeType.gas)
+        {
+            cell.gameObject.GetComponent<SpriteRenderer>().color = new Color(1f,1f, 0f);
+        }
+        else if (selectedType == ModeType.fluid)
+        {
+            cell.gameObject.GetComponent<SpriteRenderer>().color = new Color(0, 0f, 1f);
+        }
+        else if (selectedType == ModeType.solid)
+        {
+            cell.gameObject.GetComponent<SpriteRenderer>().color = new Color(0.5f, 0.5f, 0.5f);
+        }
+        else if (selectedType == ModeType.wall)
+        {
+            cell.gameObject.GetComponent<SpriteRenderer>().color = new Color(0f, 0f, 0f);
+        }
+        else if (selectedType == ModeType.delete)
+        {
+            cell.gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f);
+        }
+
+       
+    }
+
+    public void CellInfo(ref MyElementUI cell)
+    {
+        string text = "R:\t" + cell.gameObject.GetComponent<SpriteRenderer>().color.r.ToString("n4").Replace(",", ".") + System.Environment.NewLine;
+        text += "G:\t" + cell.gameObject.GetComponent<SpriteRenderer>().color.g.ToString("n4").Replace(",", ".") + System.Environment.NewLine;
+        text += "B:\t" + cell.gameObject.GetComponent<SpriteRenderer>().color.b.ToString("n4").Replace(",", ".") + System.Environment.NewLine;
+
+        int xx = cell.index / maxx;
+        int index_y = cell.index - (xx * maxx);
+        int index_x = xx;
+        text += "x:" + index_x + "\ty:" + index_y;
+
+        info_text.text = text;
+    }
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        selectedType = ModeType.none;
     }
+
+    public void SelectGas()
+    {
+        selectedType = ModeType.gas;
+    }
+
+    public void SelectFluid()
+    {
+        selectedType = ModeType.fluid;
+    }
+
+    public void SelectSolid()
+    {
+        selectedType = ModeType.solid;
+    }
+
+    public void SelectWall()
+    {
+        selectedType = ModeType.wall;
+    }
+
+    public void SelectNone()
+    {
+        selectedType = ModeType.none;
+    }
+
+    public void SelectDelete()
+    {
+        selectedType = ModeType.delete;
+    }
+
+
+
 
     public void NewTable()
     {
-        for (int x = 0; x < 80; x++)
+        
+
+        map2d = new List<MyElementUI>();
+
+        for (int x = 0; x < maxx; x++)
         {
-            for (int y = 0; y < 80; y++)
+            for (int y = 0; y < maxy; y++)
             {
                 MyElementUI myclone = Instantiate(prefab);
                 myclone.gameObject.transform.localPosition = new Vector3(dx + x * 0.11f, dy + y * 0.11f, 0);
-                myclone.gameObject.GetComponent<SpriteRenderer>().color = new Color((float)x/40f, (float)y/40f, 0);
+                //myclone.gameObject.GetComponent<SpriteRenderer>().color = new Color((float)x/40f, (float)y/40f, 0);
 
                 // myclone.transform.parent = parent.transform;
 
-                //MyElementUI myclone = Instantiate(prefab, new Vector3(x * 20.0f, y * 20.0f, 0), Quaternion.identity);
-                //myclone.transform.parent = parent.transform;
+                myclone.gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f);
+
+                int index = x * maxx + y;
+                myclone.index = index;
+                map2d.Add( myclone );
 
 
             }
--- src/MM10/Assets/MyElementUI.cs
+++ src/MM10/Assets/MyElementUI.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class MyElementUI : MonoBehaviour
 {
+    public MM10 mm;
+    public MyElementUI thiselement;
+
+    public int index ;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +23,16 @@ public class MyElementUI : MonoBehaviour
 
     public void Click()
     {
-        Debug.Log("!!!!!");
+        //Debug.Log("!!!!!");
+        mm.CellClick(ref thiselement);
+    }
+
+    public void Move()
+    {
+        mm.CellInfo(ref thiselement);
+
+        if (Input.GetMouseButton(0) == false) return;
+        //Debug.Log("Move!");
+        mm.CellClick(ref thiselement);
     }
 }
