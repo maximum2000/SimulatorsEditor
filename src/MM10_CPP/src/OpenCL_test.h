44948133cf8881b68a4176c5dba14090bd98c369
maxgammer <maxgammer@gmail.com>
2023-06-20 17:48:13 +0500

    +

--- src/MM10_CPP/src/OpenCL_test.h
+++ src/MM10_CPP/src/OpenCL_test.h
@@ -1,9 +1,34 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include<iostream>
+#include <fstream>
 
 #define __CL_ENABLE_EXCEPTIONS
 #include <CL/opencl.hpp>
+
+// Compute c = a + b.
+static const char source[] =
+"#if defined(cl_khr_fp64)\n"
+"#  pragma OPENCL EXTENSION cl_khr_fp64: enable\n"
+"#elif defined(cl_amd_fp64)\n"
+"#  pragma OPENCL EXTENSION cl_amd_fp64: enable\n"
+"#else\n"
+"#  error double precision is not supported\n"
+"#endif\n"
+"kernel void add(\n"
+"       ulong n,\n"
+"       global const double *a,\n"
+"       global const double *b,\n"
+"       global double *c\n"
+"       )\n"
+"{\n"
+"    size_t i = get_global_id(0);\n"
+"    if (i < n) {\n"
+"       c[i] = a[i] + b[i];\n"
+"    }\n"
+"}\n";
+
 
 
 int test111() {
@@ -57,7 +82,52 @@ int test111() {
 		// Create command queue.
 		cl::CommandQueue queue(context, device[0]);
 
-		
+		// Compile OpenCL program for found device.
+		cl::Program program(context, std::string(source, strlen(source)));
+
+		try {
+			program.build(device);
+		}
+		catch (const cl::Error&) {
+			std::cerr
+				<< "OpenCL compilation error" << std::endl
+				<< program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(device[0])
+				<< std::endl;
+			return 1;
+		}
+
+		cl::Kernel add(program, "add");
+
+		// Prepare input data.
+		std::vector<double> a(N, 1);
+		std::vector<double> b(N, 2);
+		std::vector<double> c(N);
+
+		// Allocate device buffers and transfer input data to device.
+		cl::Buffer A(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+			a.size() * sizeof(double), a.data());
+
+		cl::Buffer B(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+			b.size() * sizeof(double), b.data());
+
+		cl::Buffer C(context, CL_MEM_READ_WRITE,
+			c.size() * sizeof(double));
+
+		// Set kernel parameters.
+		add.setArg(0, static_cast<cl_ulong>(N));
+		add.setArg(1, A);
+		add.setArg(2, B);
+		add.setArg(3, C);
+
+		// Launch kernel on the compute device.
+		queue.enqueueNDRangeKernel(add, cl::NullRange, N, cl::NullRange);
+
+		// Get result back to host.
+		queue.enqueueReadBuffer(C, CL_TRUE, 0, c.size() * sizeof(double), c.data());
+
+		// Should get '3' here.
+		std::cout << c[42] << std::endl;
+
 	}
 	catch (const cl::Error& err) {
 		std::cerr
